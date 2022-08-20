e4c76c20709559870574468ea3c36088757d1544
meskyA <mesky2015@gmail.com>
2022-08-20 15:56:56 -0700

    first commit

---
+++ test/Intern.test.js
@@ -0,0 +1,23 @@
+// using Intern constructor 
+const Intern = require('../lib/Intern');
+
+// creating intern object  
+test('creates an Intern object', () => {
+    const intern = new Intern('Mesky', 'mesky2015@gmail', 'UCB');
+    
+    expect(intern.school) .toEqual(expect.any(String));
+});
+
+// school from getSchool()
+test('gets employee school', () => {
+    const intern = new Intern('Mesky', 77, 'mesky2015@gmail', 'UCB');
+    
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+//  role from getRole()
+test('gets role of employee', () => {
+    const intern = new Intern('Mesky', 77, 'mesky2015@gmail', 'UCB');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
---
+++ test/Manager.test.js
@@ -0,0 +1,16 @@
+// using Manager constructor 
+const Manager = require('../lib/Manager');
+
+// creating manager object  
+test('creates an Manager object', () => {
+    const manager = new Manager('Mesky', 90, 'mesky2015@gmail', 107);
+    
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// role from getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('Mesky', 77, 'mesky2015@gmail.com');
+
+    expect(manager.getRole()).toEqual("Manager");
+}); 
