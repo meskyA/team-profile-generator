5ed35e1cc68314bafc6a6d4015ab0361cac38d30
meskyA <mesky2015@gmail.com>
2022-08-21 19:48:37 -0700

    modified inter test js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,9 +1,37 @@
 // using Intern constructor 
+// const Intern = require("../lib/Intern");
+
+// test("Set school via constructor", () => {
+//   const testValue = "UCB";
+//   const e = new Intern("Mesky", 77, "mesky2015@gmail", testValue);
+//   expect(e.school).toBe(testValue);
+// });
+
+// test("getRole() should return \"Intern\"", () => {
+//   const testValue = "Intern";
+//   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+//   expect(e.getRole()).toBe(testValue);
+// });
+
+// test("Can get school via getSchool()", () => {
+//   const testValue = "UCLA";
+//   const e = new Intern("Foo", 1, "test@test.com", testValue);
+//   expect(e.getSchool()).toBe(testValue);
+// });
+
+
+
+
+
+
+
+
+
 const Intern = require('../lib/Intern');
 
 // creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Mesky', 'mesky2015@gmail', 'UCB');
+    const intern = new Intern('Mesky', 77, 'mesky2015@gmail', 'UCB');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
